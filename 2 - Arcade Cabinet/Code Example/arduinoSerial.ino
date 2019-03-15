18e05f120a8a77940cd343cfb3951b2fa21e9d49
daStroob <dario.struebin@hotmail.com>
2019-03-15 17:03:40 +0100

    Add files via upload

---
+++ 2 - Arcade Cabinet/Code Example/arduinoSerial.ino
@@ -0,0 +1,97 @@
+//Most of the code in this method comes from the Arduino and Java site:
+//http://playground.arduino.cc/Interfacing/Java
+
+const int NUMBER_OF_SHIFT_CHIPS = 2;
+
+const int DATA_WIDTH = NUMBER_OF_SHIFT_CHIPS * 8;
+
+/* You will need to change the "int" to "long" If the
+ * NUMBER_OF_SHIFT_CHIPS is higher than 2.
+*/
+
+int ploadPin        = 8;  // Connects to Parallel load pin the 165
+int clockEnablePin  = 9;  // Connects to Clock Enable pin the 165
+int dataPin        = 11; // Connects to the Q7 pin the 165
+int clockPin        = 12; // Connects to the Clock pin the 165
+
+unsigned int pinValues;
+unsigned int oldPinValues;
+
+unsigned int read_shift_regs() {
+    byte bitVal;
+    unsigned int bytesVal = 0;
+
+    /* Trigger a parallel Load to latch the state of the data lines,
+    */
+      digitalWrite(clockEnablePin, HIGH);
+      digitalWrite(ploadPin, LOW);
+      delayMicroseconds(5);
+      digitalWrite(ploadPin, HIGH);
+      digitalWrite(clockEnablePin, LOW);
+
+    /* Loop to read each bit value from the serial out line
+     * of the SN74HC165N.
+    */
+    for(int i = 0; i < DATA_WIDTH; i++) {
+        bitVal = digitalRead(dataPin);
+
+        /* Set the corresponding bit in bytesVal.
+        */
+        bytesVal |= (bitVal << ((DATA_WIDTH-1) - i));
+
+        /* Pulse the Clock (rising edge shifts the next bit).
+        */
+        digitalWrite(clockPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(clockPin, LOW);
+    }
+
+    return(bytesVal);
+}
+
+/*
+void display_pin_values() {
+    for(int i = 0; i < DATA_WIDTH; i++) {
+
+        if((pinValues >> i) & 1)
+            Serial.println(1);
+        else
+            Serial.println(0);
+    }
+}
+*/
+
+void setup() {
+    Serial.begin(9600);
+
+    /* Initialize our digital pins...
+    */
+    pinMode(ploadPin, OUTPUT);
+    pinMode(clockEnablePin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+    pinMode(dataPin, INPUT);
+
+    digitalWrite(clockPin, LOW);
+    digitalWrite(ploadPin, HIGH);
+
+    /* Read in and display the pin states at startup.
+    */
+    pinValues = read_shift_regs();
+    oldPinValues = pinValues;
+}
+
+void loop() {
+    /* Read the state of all zones.
+    */
+    pinValues = read_shift_regs();
+
+    /* If there was a change in state, display which ones changed.
+    */
+    if(pinValues != oldPinValues) {
+//        display_pin_values();
+        Serial.println(pinValues);
+        oldPinValues = pinValues;
+    }
+
+    delay(5);
+}
---
+++ 3 - Pixel Art Display/Pixel Art Display Code.ino
@@ -0,0 +1,165 @@
+#include <Adafruit_NeoPixel.h>
+#include <SD.h>
+#include <SPI.h>
+
+#define PIN 5 //LED Pin
+#define NUMPIXELS 256 //Number of Leds
+
+#define SDssPin 10    //Pin on CS
+
+#define animRuns 4
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+byte x;
+byte currentFrame;
+char currentAnimation;
+byte animCounter;
+bool animRestart;
+
+File dataFile;
+String fileName;
+
+void setup() {
+  
+  //Serial.begin(9600);
+  //while (!Serial) {}
+
+  if (!SD.begin(SDssPin)) {
+    // don't do anything more:
+    while (1);
+  }
+
+  currentFrame = 0;
+  currentAnimation = 'a';
+  animCounter = 0;
+  animRestart = false;
+
+  pixels.begin();
+}
+
+void loop() {
+  
+  fileName = currentAnimation + (String)currentFrame + ".bmp";
+
+  if (SD.exists(fileName)) {
+    animRestart = false;
+    SendFile();
+    currentFrame++;
+  } else {
+    currentFrame = 0;
+    animCounter++;
+
+    if (animRestart){
+      currentAnimation = 'a';
+      animCounter = 0;
+    }
+
+    if (animCounter >= animRuns) {
+      currentAnimation++;
+      animCounter = 0;
+      animRestart = true;
+    }
+  }
+  
+}
+
+
+void SendFile() {
+  //char temp[14];
+  //Filename.toCharArray(temp, 14);
+
+  dataFile = SD.open(fileName);
+
+  if (dataFile) {
+    ReadTheFile();
+    dataFile.close();
+  }
+  else {
+    delay(1000);
+    //setupSDcard();
+    return;
+  }
+}
+
+
+uint32_t readLong() {
+  uint32_t retValue;
+  byte incomingbyte;
+
+  incomingbyte = readByte();
+  retValue = (uint32_t)((byte)incomingbyte);
+
+  incomingbyte = readByte();
+  retValue += (uint32_t)((byte)incomingbyte) << 8;
+
+  incomingbyte = readByte();
+  retValue += (uint32_t)((byte)incomingbyte) << 16;
+
+  incomingbyte = readByte();
+  retValue += (uint32_t)((byte)incomingbyte) << 24;
+
+  return retValue;
+}
+
+
+uint16_t readInt() {
+  byte incomingbyte;
+  uint16_t retValue;
+
+  incomingbyte = readByte();
+  retValue += (uint16_t)((byte)incomingbyte);
+
+  incomingbyte = readByte();
+  retValue += (uint16_t)((byte)incomingbyte) << 8;
+
+  return retValue;
+}
+
+
+int readByte() {
+  int retbyte = -1;
+  while (retbyte < 0) retbyte = dataFile.read();
+  return retbyte;
+}
+
+
+void ReadTheFile() {
+#define MYBMP_BF_TYPE           0x4D42
+#define MYBMP_BF_OFF_BITS       54
+#define MYBMP_BI_SIZE           40
+#define MYBMP_BI_RGB            0L
+#define MYBMP_BI_RLE8           1L
+#define MYBMP_BI_RLE4           2L
+#define MYBMP_BI_BITFIELDS      3L
+
+#define displayWidth    16
+#define lineLength      48
+
+  for (int y = 0; y < 16; y++) {
+    int bufpos = 0;
+    for (int x = 0; x < 16; x++) {
+      uint32_t offset = (MYBMP_BF_OFF_BITS + ((y * lineLength) + (x * 3))) ;
+      dataFile.seek(offset);
+
+      //getRGBwithGamma();
+      byte b = readByte();
+      byte g = readByte();
+      byte r = readByte();
+
+      int activePixel;
+      if (y%2 == 0) {
+        activePixel = (y+1)*16 -x -1;
+      } else {
+        activePixel = y*16 + x;
+      }
+
+      pixels.setPixelColor(activePixel, pixels.Color(r,g,b));
+      //printPixel(y);
+
+    }
+    
+  }
+
+  pixels.show();
+}
